363b961558d65423ef1ac033faf6f18c08e6b014
MikkoMalinen <mikko.i.malinen@gmail.com>
2026-01-17 22:22:11 +0200

    Add files via upload
    Now also a possibility not to draw convex hulls.

--- test_kmeans_inf.m
+++ test_kmeans_inf.m
@@ -1,21 +1,24 @@
 % Testing infinity norm cost clustering
 % Created by Microsoft Copilot
 % Modified by Mikko Malinen, 2025
+% Version 0.03
 
 clear all;
 
 tic;
 
-REPEATS = 10;
+REPEATS = 1;
+DRAW_CONVEX_HULL = 1;  % 0 = no, 1 = yes
+
 Best_cost_L_infty = Inf;
 
 % Generate sample 2D data
 %X = [randn(50,2)+2; randn(50,2)-2; randn(50,2)+[5 -3]];
 
 %X = load('s2.txt'); k = 15;
-%X = load('../datasets/s2.txt'); k = 15;
+X = load('../datasets/s2.txt'); k = 15;
 %X = load('../datasets/birch1.txt'); k = 100;
-X = load('../datasets/iris.txt'); k = 3;
+%X = load('../datasets/iris.txt'); k = 3;
 
 n = size(X,1);
 d = size(X,2);
@@ -42,6 +45,7 @@ end % repeats
 
 toc;
 
+if DRAW_CONVEX_HULL==1
 sz = zeros(k,1);  % sizes of clusters
 for i = 1:n
     sz(idx_best(i)) = sz(idx_best(i))+1;
@@ -52,39 +56,43 @@ for j = 1:k
     CM_2(1:sz(j),1:2) = CM(j,1:sz(j),1:2);
     clear idx_clust;
     [idx_clust, vol] = convhulln(CM_2);
-if j==1
-    idx_b_clust1 = idx_clust(:,1);
-elseif j==2
-    idx_b_clust2 = idx_clust(:,1);
-elseif j==3
-    idx_b_clust3 = idx_clust(:,1);
-end
+    idx_b_clust{j} = idx_clust(:,1);
+    %if j==1
+    %    idx_b_clust1 = idx_clust(:,1);
+    %elseif j==2
+    %    idx_b_clust2 = idx_clust(:,1);
+    %elseif j==3
+    %    idx_b_clust3 = idx_clust(:,1);
+    %end
 
 end
-
+end % DRAW_CONVEX_HULL
 
 % Plot results
 figure; hold on;
 gscatter(X(:,1), X(:,2), idx);
 plot(centers(:,1), centers(:,2), 'kx', 'MarkerSize', 12, 'LineWidth', 2);
 title('k-Means with L-infinity norm');
-for j=1:k   %1:k
+if DRAW_CONVEX_HULL==1
+for j=1:k   
     hold on;
     clear CH;
     clear CM_2;
     CM_2(1:sz(j),1:2) = CM(j,1:sz(j),1:2);
-    if j==1
-        CH(:,1:2) = CM_2(idx_b_clust1,1:2);
-    elseif j==2
-        CH(:,1:2) = CM_2(idx_b_clust2,1:2);
-    elseif j==3
-        CH(:,1:2) = CM_2(idx_b_clust3,1:2);
-    end
+    CH(:,1:2) = CM_2(idx_b_clust{j},1:2);
+    %if j==1
+    %    CH(:,1:2) = CM_2(idx_b_clust1,1:2);
+    %elseif j==2
+    %    CH(:,1:2) = CM_2(idx_b_clust2,1:2);
+    %elseif j==3
+    %    CH(:,1:2) = CM_2(idx_b_clust3,1:2);
+    %end
     plot(CH(:,1),CH(:,2),'k');
     CH_last(1,:) = CH(end,1:2);
     CH_last(2,:) = CH(1,1:2);
     hold on;
     plot(CH_last(:,1),CH_last(:,2),'k');
 end
+end % DRAW_CONVEX_HULL
 
 Best_cost_L_infty = Best_cost_L_infty 
